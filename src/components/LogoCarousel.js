0b49e0662567cd8c3e8c7be74030e15c442c1dcd
Lenanor <nordstromlena@hotmail.com>
2026-01-18 21:46:43 +0100

    change text and image, fix carousel

--- src/components/LogoCarousel.js
+++ src/components/LogoCarousel.js
@@ -4,7 +4,7 @@ import styles from "./LogoCarousel.module.css";
 function LogoCarousel() {
   return (
     <section className={styles.logos}>
-      {Array(3)
+      {Array(4)
         .fill(null)
         .map((_, index) => (
           <div className={styles.logosSlide} key={index}>
